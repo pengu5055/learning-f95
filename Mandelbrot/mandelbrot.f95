a493696fa7b29745686b164aa78c140b65661cfc
Marko Urbanc <urbancmarko1@gmail.com>
2024-12-11 15:00:01 +0100

    feat: :sparkles: optimize fortran demo code

--- Mandelbrot/mandelbrot.f95
+++ Mandelbrot/mandelbrot.f95
@@ -1,3 +1,12 @@
+function abs2(x) result(value)
+    use types, only: dp
+    implicit none
+    complex(dp), intent(in) :: x
+    real(dp) :: value
+
+    value = real(x, dp)**2 + aimag(x)**2
+end function abs2
+
 program Mandelbrot
     use types, only: dp
     use constants, only: I
@@ -5,28 +14,38 @@ program Mandelbrot
     use mesh, only: linspace, meshgrid
     implicit none
 
-
+    real(dp) :: abs2
     integer, parameter :: ITER = 1000
     integer, parameter :: DENSITY = 1000
-    real(dp) :: x_min, x_max, y_min, y_max
+    real(dp) :: x_min, x_max, y_min, y_max, bound
     real(dp), dimension(DENSITY, DENSITY) :: x, y
     complex(dp), dimension(DENSITY, DENSITY) :: c, z
     integer, dimension(DENSITY, DENSITY) :: fractal
-    integer :: n
+    integer :: n, m, l
     x_min = -2.68_dp
     x_max = 1.32_dp
     y_min = -1.5_dp
     y_max = 1.5_dp
+    bound = 100.0_dp
 
     call meshgrid(linspace(x_min, x_max, DENSITY), linspace(y_min, y_max, DENSITY), x, y)
     c = x + I*y
     z = c 
     fractal = 255
 
-    do n = 1, ITER
+    do l = 1, ITER
         print "('Iteration ', i0)", n
-        where (abs(z) <= 10) z = z**2 + c
-        where (fractal == 255 .and. abs(z) > 10) fractal = 254 * (n - 1) / ITER
+        do n = 1, DENSITY
+            do m = 1, DENSITY
+                if (fractal(n, m) == 255) then
+                    if (abs2(z(n, m)) > bound) then
+                        fractal(n, m) = 254 * (n - 1) / ITER
+                    end if
+                else if (abs2(z(n, m)) <= bound) then
+                    z(n, m) = z(n, m)**2 + c(n, m)
+                end if
+            end do
+        end do
     end do
 
     print *, 'Saving... '
